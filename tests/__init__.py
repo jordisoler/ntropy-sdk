d970538e021ae9f01cabdafc0e1f1d4ee1bf5200
David Buchmann <david.buchmann@gmail.com>
2021-05-12 10:54:03 +0200

    Initial version

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Unit test package for ntropy_sdk."""
---
+++ tests/test_ntropy_sdk.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `ntropy_sdk` package."""
+
+
+import unittest
+
+from ntropy_sdk import ntropy_sdk
+
+
+class TestNtropy_sdk(unittest.TestCase):
+    """Tests for `ntropy_sdk` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
